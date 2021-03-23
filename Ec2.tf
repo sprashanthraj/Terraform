d858c47409c14cafa83cd1e4bac73d3d4e80e2a7
raju <sprasanthraj6768@gmail.com>
2021-03-23 20:38:18 +0000

    Adding more files

---
+++ Ec2.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "default"
+    region  = "ap-south-1"  
+}
+
+resource "aws_instance"  "raj" {
+    ami           = "ami-068d43a544160b7ef"
+    instance_type = "t2.micro"
+    
+    tags = {
+      key_name  = "Sample Instance"
+    }
+}
