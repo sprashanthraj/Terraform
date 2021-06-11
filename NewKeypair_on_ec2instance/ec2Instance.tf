aad71b234bb50d696424146eb2b55dec728c2db9
raju <sprasanthraj6768@gmail.com>
2021-06-11 22:46:31 +0100

    New roles

---
+++ NewKeypair_on_ec2instance/ec2Instance.tf
@@ -0,0 +1,24 @@
+# New ec2 instance with existing key pair and also creating new key pair and referring it to existing ec2 instance.
+
+provider "aws" {
+  region = "us-east-2"
+  
+}
+
+# creating a new key pair and attaching the key pair to ec2 instance
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/terraform.pub")
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+
+  tags = {
+    Name = "Terraform"
+    Batch = "7AM"
+  }
+}
