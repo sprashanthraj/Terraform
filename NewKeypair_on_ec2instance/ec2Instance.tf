a5e0ac44a750e4d1c5fea813607d0de7ec3f9519
raju <sprasanthraj6768@gmail.com>
2021-06-15 19:35:20 +0100

    updates

--- NewKeypair_on_ec2instance/ec2Instance.tf
+++ NewKeypair_on_ec2instance/ec2Instance.tf
@@ -2,7 +2,7 @@
 
 provider "aws" {
   region = "us-east-2"
-  
+
 }
 
 # creating a new key pair and attaching the key pair to ec2 instance
@@ -15,10 +15,11 @@ resource "aws_key_pair" "deployer" {
 resource "aws_instance" "web" {
   ami           = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.deployer.key_name
+  user_data     = file("user_data.sh")
 
   tags = {
-    Name = "Terraform"
+    Name  = "Terraform"
     Batch = "7AM"
   }
 }
